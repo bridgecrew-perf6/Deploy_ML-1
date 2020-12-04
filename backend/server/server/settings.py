995f1f753f6163147d80615a15bda67d49c5f38c
ghousethanedar <ghousethanedar@gmail.com>
2020-12-04 22:23:18 +0530

    docker files added

--- backend/server/server/settings.py
+++ backend/server/server/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'awux!24$()eufx*b&=e%zmr$lthn!vndi43gr*skv@s9ilmlb7'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['0.0.0.0']
 
 
 # Application definition
@@ -122,3 +122,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
